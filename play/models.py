7793e12d142be196afba0b7cbced0fbcd39f989c
MBB <pynchia@gmail.com>
2015-01-21 23:31:21 +0100

    first hint with gw models

--- play/models.py
+++ play/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+SUBJECT_NAME = 32
+FEATURE_DESCR = 128
+
+
+class Player(models.Model):
+    user = models.OneToOneField(User)
+    games_played = models.IntegerField(default=0)
+    games_won = models.IntegerField(default=0)
+
+
+class Game(models.Model):
+    player = models.ForeignKey(Player)
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=SUBJECT_NAME)
+
+
+class Feature(models.Model):
+    subject = models.ManyToManyField(Subject)
+    description = models.CharField(max_length=FEATURE_DESCR)
+
+
+class BoardElement(models.Model):
+    subject = models.ForeignKey(Subject)
+
+
