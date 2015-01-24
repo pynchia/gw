2e53a8ee0ce701d50083cdc137919e1ef82b07b5
MBB <pynchia@gmail.com>
2015-01-25 00:33:28 +0100

    registered models on the admin

--- play/admin.py
+++ play/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Player, Game, Subject, Feature, BoardElement
+
 
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Game)
+admin.site.register(Subject)
+admin.site.register(Feature)
+admin.site.register(BoardElement)
+
