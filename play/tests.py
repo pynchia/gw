c821fbbf9fbeb3ef3dbb6a7a98dd40895e32268d
MBB <pynchia@gmail.com>
2015-04-21 22:39:08 +0200

    added tests, but not working...

--- play/tests.py
+++ play/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from play.models import Subject, Feature, Player, Game, BoardElement
 
-# Create your tests here.
+
+class TestFeatures(TestCase):
+
+    def setUp(self):
+        self.subj = Subject.objects.get(pk=1)  # Alex
+
+    def test_alex_features(self):
+        n = self.subj.feature_set.count()
+        self.assertEqual(n, 6)
+
