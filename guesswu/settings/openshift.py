323825e9d659267eb951ecc6540a74f78e3bfa1b
MBB <pynchia@gmail.com>
2015-09-08 12:28:29 +0200

    added settings for openshift

---
+++ guesswu/settings/openshift.py
@@ -0,0 +1,31 @@
+# settings/local.py
+from .base import *
+
+SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': get_env_variable("OPENSHIFT_POSTGRESQL_DB_HOST"),
+        'PORT': get_env_variable("OPENSHIFT_POSTGRESQL_DB_PORT"),
+        'NAME': get_env_variable("guesswu"),
+        'USER': get_env_variable("DJANGO_DB_USER"),
+        'PASSWORD': get_env_variable("DJANGO_DB_PWD"),
+    }
+}
+
+INSTALLED_APPS += ("debug_toolbar", )
+INTERNAL_IPS = ("127.0.0.1",)
+MIDDLEWARE_CLASSES += \
+        ("debug_toolbar.middleware.DebugToolbarMiddleware", )
+
